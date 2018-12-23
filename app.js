c1b92503bf80b3da69ca66180f12d4af8d01a55b
dev-chulbuji <dev.chulbuji@gmail.com>
2018-12-23 15:57:26 +0900

    set access log

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 
+const logger = require('morgan')
+const path = require('path')
+const fs = require('fs')
+
+const setLogger = () => {
+  app.use(logger('dev'))
+
+  const logFile = fs.createWriteStream(path.normalize(`${__dirname}/log/access.log`), { flags: 'a' })
+  const format = ':remote-addr [:date[iso]] ":method :url HTTP/:http-version" :status' +
+    ' :res[content-length] ":referrer" ":user-agent" :response-time'
+  app.use(logger(format, { stream: logFile, skip: false }))
+
+}
+
+setLogger()
+
 app.get('*', (req, res, next) => {
   res.json('test response')
 })
